46940224d34b0110adbcffa55abe432b4151a86c
Chojay2 <chojay.wangchuk@selise.ch>
2023-05-17 18:10:32 +0600

    init commit
    Signed-off-by: Chojay2 <chojay.wangchuk@selise.ch>

---
+++ js/script.js
@@ -0,0 +1,66 @@
+const track = document.getElementById("image-track");
+
+const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
+
+const handleOnUp = () => {
+  track.dataset.mouseDownAt = "0";  
+  track.dataset.prevPercentage = track.dataset.percentage;
+}
+
+
+//TODO:- might need later for scroll effect
+// const swapMove = e => {
+//     // console.log(e.deltaX)
+//     track.dataset.mouseDownAt = e.deltaX;
+//     if(track.dataset.mouseDownAt === "0") return;
+   
+//     track.animate({
+//       transform: `translate(${nextPercentage}%, -50%)`
+//     }, { duration: 1200, fill: "forwards" });
+    
+//     for(const image of track.getElementsByClassName("image")) {
+//       image.animate({
+//         objectPosition: `${100 + nextPercentage}% center`
+//       }, { duration: 1200, fill: "forwards" });
+//     }
+
+// } 
+
+
+const handleOnMove = e => {
+  if(track.dataset.mouseDownAt === "0") return;
+
+  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+        maxDelta = window.innerWidth / 3;
+  const percentage = (mouseDelta / maxDelta) * -100,
+        nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
+        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+        track.dataset.percentage = nextPercentage;
+  
+
+  track.animate({
+    transform: `translate(${nextPercentage}%, -50%)`
+  }, { duration: 1200, fill: "forwards" });
+  
+  for(const image of track.getElementsByClassName("image")) {
+    image.animate({
+      objectPosition: `${100 + nextPercentage}% center`
+    }, { duration: 1200, fill: "forwards" });
+  }
+}
+
+
+
+window.onmousedown = e => handleOnDown(e);
+
+window.ontouchstart = e => handleOnDown(e.touches[0]);
+
+window.onmouseup = e => handleOnUp(e);
+
+window.ontouchend = e => handleOnUp(e.touches[0]);
+
+window.onmousemove = e => handleOnMove(e);
+
+window.ontouchmove = e => handleOnMove(e.touches[0]);
+
+track.onwheel = e => swapMove(e);
